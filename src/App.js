1c876750aae41e790a71748ed3b4f253e10c0dc0
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-01-28 23:11:42 +0300

    created react context to manage a light/dark mode

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import { About } from "./components/about/About";
 import { ProductList } from "./components/productList/ProductList";
 import { Contact } from "./components/contact/Contact";
 import { Toggle } from "./components/toggle/Toggle";
+import { useContext } from "react";
+import { ThemeContext } from "./context";
 
 function App() {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
   return (
-    <div>
+    <div
+      style={{
+        backgroundcolor: darkMode ? "#222" : "white",
+        color: darkMode && "white",
+      }}
+    >
       <Toggle />
       <Intro />
       <About />
