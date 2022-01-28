90f62a368587fe8feee95ae79c67fb9d36c6ccff
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-01-28 22:22:43 +0300

    created component: Toggle.jsx + css

---
+++ src/components/toggle/Toggle.jsx
@@ -0,0 +1,13 @@
+import "./toggle.css";
+import Sun from "../../img/sun.png";
+import Moon from "../../img/moon.png";
+
+export const Toggle = () => {
+  return (
+    <div className="t">
+      <img src={Sun} alt="" className="t-icon" />
+      <img src={Moon} alt="" className="t-icon" />
+      <div className="t-button"></div>
+    </div>
+  );
+};
