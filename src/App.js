f9b4d639f0ee25b4436c5d0c9c5b3ecd753f997a
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-01-28 21:34:53 +0300

    created component: Contact.jsx, installed and configured EmailJS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Intro } from "./components/intro/Intro";
 import { About } from "./components/about/About";
 import { ProductList } from "./components/productList/ProductList";
+import { Contact } from "./components/contact/Contact";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Intro />
       <About />
       <ProductList />
+      <Contact />
     </div>
   );
 }
